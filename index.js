f34f85a826f354b6ca657888867bba944ab85f38
Sarwar Azmain Eyafi <75948244+EyafiNeo@users.noreply.github.com>
2022-12-05 12:23:06 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+// bobshopuser
+// V0pBHvh3HzUQHetw
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bz2bbta.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+  try {
+    const productsCollection = client.db("bobshop").collection("products");
+
+    app.get('/products', async(req,res)=>{
+        const query = {};
+        const result = await productsCollection.find(query).toArray();
+        res.send(result)
+    })
+
+  } finally {
+  }
+};
+
+run().catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Bob Shop server listening on port ${port}`);
+});
